3100a3f69f87be71b48d4419cff20623f6ddb6f7
Felipe Soller <soller@outlook.com>
2024-03-29 15:23:04 -0300

    create auth context for the app

--- App.tsx
+++ App.tsx
@@ -1,24 +1,23 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading'
+import { AuthContextProvider } from '@contexts/AuthContext'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Routes } from '@routes/index'
+import { NativeBaseProvider } from 'native-base'
+import { StatusBar } from 'react-native'
 
-import { Routes } from '@routes/index';
-
-import { THEME } from './src/theme';
-
-import { Loading } from '@components/Loading';
+import { THEME } from './src/theme'
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <AuthContextProvider>
+        {fontsLoaded ?
+          <Routes />
+        : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
-  );
+  )
 }
